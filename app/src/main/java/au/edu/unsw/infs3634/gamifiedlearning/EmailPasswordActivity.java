c257ee8741df94d13207f5e3b7959765878c88b0
tristanbagnulo <tristanbagnulo1@gmail.com>
2020-11-06 17:08:20 +1100

    Login stufff

---
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/EmailPasswordActivity.java
@@ -0,0 +1,33 @@
+package au.edu.unsw.infs3634.gamifiedlearning;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class EmailPasswordActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_password);
+
+        //Initialise Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+}
--- app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/MainActivity.java
@@ -2,13 +2,35 @@ package au.edu.unsw.infs3634.gamifiedlearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btDetailActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btDetailActivity = (Button) findViewById(R.id.btDetailActivity);
+        btDetailActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDetailActivity();
+
+            }
+        });
+
     }
+
+    public void openDetailActivity(){
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
+    }
+
+
 }
