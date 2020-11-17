f372acfedcc3ca37b25a82c6d6b90416b9486a3d
z5265210 <62052161+z5265210@users.noreply.github.com>
2020-11-17 20:37:52 +1100

    now with updated question

--- app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/QuestionsAndAnswerSet.java
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/QuestionsAndAnswerSet.java
@@ -58,6 +58,7 @@ public class QuestionsAndAnswerSet {
         ArrayList<QuestionsAndAnswerSet> questionsAndAnswerSets = new ArrayList<>();
         questionsAndAnswerSets.add(new QuestionsAndAnswerSet("What is the key predictor of your cardio vascular health?", "Resting Heart Rate", "Heart Rate", "Heart Size","Resting Heart Rate"));
         questionsAndAnswerSets.add(new QuestionsAndAnswerSet("What hormone is released you get 20-30 minutes of aerobic activity?", "Dopamine", "Serotonin", "BDNF","BDNF"));
+        questionsAndAnswerSets.add(new QuestionsAndAnswerSet("What age are you at high risk of heart disease?", "Under 20", "Between 20 and 45", "Over 45","Over 45"));
     return questionsAndAnswerSets;
     }
 }
